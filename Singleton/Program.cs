f35894f3d4fb57a486b8d75323dc750c5390fe60
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-12-27 19:22:09 +0530

    Adding Singleton example

---
+++ Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(FirstInvoke, SecondInvoke);
+            //Console.WriteLine(new Singleton().RandomNum);
+            //Console.WriteLine(new Singleton().RandomNum);
+            Console.ReadLine();
+        }
+
+        private static void SecondInvoke()
+        {
+            Console.WriteLine(Singleton.GetInstance().RandomNum);
+        }
+
+        private static void FirstInvoke()
+        {
+            Console.WriteLine(Singleton.GetInstance().RandomNum);
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        public int RandomNum { get; set; }
+        public static Singleton instance;
+        static readonly object _object = new object();
+        private Singleton()
+        {
+            RandomNum = new Random().Next(999999999);
+        }
+
+        //public Singleton()
+        //{
+        //    RandomNum = new Random().Next(999999999);
+        //}
+
+        public static Singleton GetInstance()
+        {
+            Monitor.Enter(_object);
+            try
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+            }
+            finally
+            {
+                Monitor.Exit(_object);
+            }
+
+            //lock(_object)
+            //{
+            //    if (instance == null)
+            //    {
+            //        instance = new Singleton();
+            //    }
+            //}
+
+            return instance;
+        }
+    }
+}
